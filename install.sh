2334f88cd2e02aa589d366b2fcc01a786b0a7117
Matthew Boyea <contact@mboyea.com>
2025-08-27 14:17:02 -0500

    install script asks for root privilege; disabling pam_ecryptfs no longer generates duplicate .bak files; disabling startup splash screen no longer overwrites other GRUB_CMDLINE_LINUX_DEFAULT config; draft set cinnamon theme settings; draft home-manager common.nix

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-echo 'TODO: remove early exit'
-exit
-
-# TODO: sudo su
+if [[ $EUID -ne 0 ]]; then
+  echo -e '\e[1mThis installation script requires root privileges.\e[0m'
+  exec sudo bash "$0" "$@"
+  exit 1
+fi
 
 echo -e '\e[1mUpdating package cache...\e[0m'
+
 apt update # ! sudo
 
 echo -e '\e[1mInstalling greetd+tuigreet...\e[0m'
@@ -13,21 +15,36 @@ echo -e '\e[1mInstalling greetd+tuigreet...\e[0m'
 nix-shell -p cargo --command 'cargo build --release --targetdir src/tuigreet'
 cp -f src/tuigreet/target/release/tuigreet /usr/local/bin/ # ! sudo
 apt install greetd # ! sudo
-# ? casper-md5check causes grub to refuse to boot if it detects changes to the login process (required to change the DM to greetd)
+# ? casper-md5check causes the OS to refuse to boot if it detects changes to the login process (required to change the DM to greetd)
 systemctl disable casper-md5check # ! sudo
 systemctl disable lightdm.service # ! sudo
 if ! systemctl is-enabled greetd.service | grep -q 'enabled'; then
   systemctl enable greetd.service # ! sudo
 fi
 mkdir -p /etc/greetd # ! sudo
-cp -f static/etc/greetd/conf.toml /etc/greetd # ! sudo
-# ? pam_ecryptfs can sometimes cause greetd to fail to boot, as they are incompatible; ecryptfs is deprecated anyways
-for file in /etc/pam.d/*; do
-  sed -i.bak '/^[^#]*pam_ecryptfs/s/^/# /' "$file"
-done
+cp -f src/root/etc/greetd/conf.toml /etc/greetd # ! sudo
+# ? pam_ecryptfs can sometimes cause greetd to fail to boot, so it is disabled here; Ubuntu considers ecryptfs to be deprecated anyways
+find /etc/pam.d -type f -not -name '*.bak' -print0 \
+  | xargs -0r grep -lZ '^[^#]*pam_ecryptfs' \
+  | xargs -0r sed -i'.bak' '/^[^#]*pam_ecryptfs/s/^/# /' # ! sudo
 
 echo -e '\e[1mDisabling startup splash screen...\e[0m'
 
-sed -i.bak '/GRUB_CMDLINE_LINUX_DEFAULT/s/^.*$/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub # ! sudo
+sed -i.bak '/GRUB_CMDLINE_LINUX_DEFAULT/s/quiet\|splash//g' /etc/default/grub # ! sudo
 update-grub # ! sudo
+
+echo -e '\e[1mTODO: Installing i3wm...\e[0m'
+
+echo -e '\e[1mTODO: Installing Nix Home Manager\e[0m'
+
+echo -e '\e[1mConfiguring Cinnamon...\e[0m'
+
+gsettings set org.cinnamon.desktop.interface icon-theme 'Mint-Y-Sand'
+gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark-Aqua'
+gsettings set org.cinnamon.desktop.interface gtk-theme-backup 'Adwaita'
+gsettings set org.cinnamon.theme name 'cinnamon'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+echo -e '\e[1mTODO: Configuring i3wm...\e[0m'
+
 
