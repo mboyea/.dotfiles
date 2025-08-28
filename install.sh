915c99aaa38a8ea90c09b39f391275aebb5bd11c
Matthew Boyea <contact@mboyea.com>
2025-08-28 14:56:56 -0500

    install i3 and hide special session types from tuigreet

--- install.sh
+++ install.sh
@@ -10,6 +10,10 @@ echo -e '\e[1mUpdating package cache...\e[0m'
 
 apt update # ! sudo
 
+echo -e '\e[1mInstalling i3wm...\e[0m'
+
+apt install i3 # ! sudo
+
 echo -e '\e[1mInstalling greetd+tuigreet...\e[0m'
 
 nix-shell -p cargo --command 'cargo build --release --targetdir src/tuigreet'
@@ -33,15 +37,17 @@ cp -f src/root/etc/greetd/conf.toml /etc/greetd # ! sudo
 find /etc/pam.d -type f -not -name '*.bak' -print0 \
   | xargs -0r grep -lZ '^[^#]*pam_ecryptfs' \
   | xargs -0r sed -i'.bak' '/^[^#]*pam_ecryptfs/s/^/# /' # ! sudo
+# ? hide special session configurations
+mkdir -p /usr/share/backup
+cp -r /usr/share/xsessions /usr/share/wayland-sessions /usr/share/backup
+rm -f /usr/share/xsessions/cinnamon2d.desktop
+rm -f /usr/share/xsessions/i3-with-shmlog.desktop
+rm -f /usr/share/wayland-sessions/cinnamon-wayland.desktop
 
-echo -e '\e[1mDisabling startup splash screen...\e[0m'
+echo -e '\e[1mEnabling boot log screen...\e[0m'
 
 sed -i.bak '/GRUB_CMDLINE_LINUX_DEFAULT/s/quiet\|splash//g' /etc/default/grub # ! sudo
 update-grub # ! sudo
-
-echo -e '\e[1mInstalling i3wm...\e[0m'
-
-apt install i3 # ! sudo
 
 echo -e '\e[1mInstalling Nix Home Manager...\e[0m'
 
@@ -49,7 +55,7 @@ nix-channel --add https://github.com/nix-community/home-manager/archive/master.t
 nix-channel --update
 nix-shell '<home-manager>' -A install
 
-echo -e '\e[1mConfiguring Cinnamon...\e[0m'
+echo -e '\e[1mConfiguring Desktops...\e[0m'
 
 gsettings set org.cinnamon.desktop.interface icon-theme 'Mint-Y-Sand'
 gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark-Aqua'
@@ -57,6 +63,5 @@ gsettings set org.cinnamon.desktop.interface gtk-theme-backup 'Adwaita'
 gsettings set org.cinnamon.theme name 'cinnamon'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
-echo -e '\e[1mTODO: Configuring i3wm...\e[0m'
-
+# todo: home-manager switch
 
