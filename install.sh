c49a9522d131e78a4f61ecfb829688fc50e7ac4a
Matthew Boyea <contact@mboyea.com>
2025-09-26 01:02:48 -0500

    fix boot logs wont overlap with greeter; fix hide startx output on cinnamon startup

--- install.sh
+++ install.sh
@@ -47,15 +47,15 @@ cp -f "$SCRIPT_DIR/root/etc/systemd/system/greetd.service.d/override.conf" /etc/
 # ? pam_ecryptfs can sometimes cause greetd to fail to boot, so it is disabled here; Ubuntu considers ecryptfs to be deprecated anyways
 find /etc/pam.d -type f -not -name '*.bak' -print0 \
   | xargs -0r grep -lZ '^[^#]*pam_ecryptfs' \
-  | xargs -0r sed -i'.bak' '/^[^#]*pam_ecryptfs/s/^/# /' # ! sudo
+  | xargs -0r sed -i.bak '/^[^#]*pam_ecryptfs/s/^/# /' # ! sudo
 # ? hide special session configurations
 mkdir -p /usr/share/backup # ! sudo
 cp -r /usr/share/xsessions /usr/share/wayland-sessions /usr/share/backup # ! sudo
 rm -f /usr/share/xsessions/cinnamon2d.desktop # ! sudo
 rm -f /usr/share/xsessions/i3-with-shmlog.desktop # ! sudo
 rm -f /usr/share/wayland-sessions/cinnamon-wayland.desktop # ! sudo
-sed '/^Exec=[^>]*$/s/$/ > \/dev\/null 2>&1/' /usr/share/xsessions/i3.desktop # ! sudo
-sed '/^Exec=[^>]*$/s/$/ > \/dev\/null 2>&1/' /usr/share/xsessions/cinnamon.desktop # ! sudo
+sed -i '/^Exec=[^>]*$/s/$/ > \/dev\/null 2>&1/' /usr/share/xsessions/i3.desktop # ! sudo
+sed -i '/^Exec=[^>]*$/s/$/ > \/dev\/null 2>&1/' /usr/share/xsessions/cinnamon.desktop # ! sudo
 
 echo -e '\e[1mEnabling boot log screen...\e[0m'
 
