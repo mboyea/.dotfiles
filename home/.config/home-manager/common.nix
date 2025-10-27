0ecdce778156c28bb271cf68c1d428b8ab52300a
Matthew Boyea <contact@mboyea.com>
2025-10-27 09:21:58 -0500

    fix home manager nix config

--- home/.config/home-manager/common.nix
+++ home/.config/home-manager/common.nix
@@ -1,6 +1,4 @@
 { config, lib, pkgs, inputs, ... }: {
-  nixGL.packages = import <nixgl> { inherit pkgs; };
-
   nixpkgs = {
     config = {
       allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
@@ -20,11 +18,11 @@
     (config.lib.nixGL.wrap discord) 
     (config.lib.nixGL.wrap spotify) 
     (config.lib.nixGL.wrap vlc)
-    (config.lib.nixGL.wrap libreoffice)
+    # (config.lib.nixGL.wrap libreoffice) # can't install due to cache miss + 5hr build
     (config.lib.nixGL.wrap gimp)
     (config.lib.nixGL.wrap inkscape)
     (config.lib.nixGL.wrap audacity)
-    # (config.lib.nixGL.wrap lmms)
+    # (config.lib.nixGL.wrap lmms) # can't install due to cache miss + build failure
     (config.lib.nixGL.wrap obs-studio)
   ];
 
